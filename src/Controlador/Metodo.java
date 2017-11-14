c7d319e63d201a773a3c0850f83f1181397cf8c2
carhe <carhe@Carlos>
2017-11-14 20:25:09 -0300

    Testing 1.5

--- src/Controlador/Metodo.java
+++ src/Controlador/Metodo.java
@@ -6,6 +6,7 @@
 package Controlador;
 
 import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,16 +17,17 @@ import javax.swing.table.DefaultTableModel;
  * @author Carlos Herrera
  */
 public class Metodo {
-    
+
     /**
-     * Método que devuelve la tabla de JedeDepto 
+     * Método que devuelve la tabla de JedeDepto
+     *
      * @return Modelo de la tabla completa con registros
      */
     public static DefaultTableModel CargarJefe() {
         String sql = "SELECT J.RUT , J.NOMBRE, J.APELLIDO, J.TELEFONO, J.ESTADO, "
                 + "J.NOMBREDEPTO, J.SUELDO FROM JEFEDEPTO J";
-        String[] titulos = {"Rut","Nombre","Apellido","Telefono","Estado",
-            "Departamento","Sueldo"};
+        String[] titulos = {"Rut", "Nombre", "Apellido", "Telefono", "Estado",
+            "Departamento", "Sueldo"};
         DefaultTableModel model = new DefaultTableModel(null, titulos);
         String[] fila = new String[7];
         try {
@@ -37,7 +39,7 @@ public class Metodo {
                 fila[1] = rs.getString("NOMBRE");
                 fila[2] = rs.getString("APELLIDO");
                 fila[3] = rs.getString("TELEFONO");
-                if(rs.getInt("ESTADO")==1){
+                if (rs.getInt("ESTADO") == 1) {
                     fila[4] = "ACTIVO";
                 } else {
                     fila[4] = "INACTIVO";
@@ -53,10 +55,12 @@ public class Metodo {
             return model;
         }
     }
-    
+
     /**
      * Método llama al procedimiento de CRUD de JefeDepto y devuelve Booleano
-     * @param OPT Opcion crud (1=insertar, 2=actualizar, 3=eliminar jefe, 4=resaurar jefe)
+     *
+     * @param OPT Opcion crud (1=insertar, 2=actualizar, 3=eliminar jefe,
+     * 4=resaurar jefe)
      * @param RUT Rut Jefe de Departamento
      * @param NOMBRE Nombre Jefe de Departamento
      * @param APELLIDO Apellido Jefe de Departamento
@@ -64,8 +68,8 @@ public class Metodo {
      * @param DEPARTAMENTO Nombre departamento en el cual trabaja
      * @param SUELDO Sueldo Jefe de Departamento
      * @return Boolean
-     */    
-    public static boolean CrudJefe(int OPT, String RUT, String NOMBRE, String APELLIDO, 
+     */
+    public static boolean CrudJefe(int OPT, String RUT, String NOMBRE, String APELLIDO,
             String TELEFONO, String DEPARTAMENTO, int SUELDO) {
         try {
             CallableStatement PROCEDURE = Conexion.conectar().prepareCall("{call JEFE_CRUD (?,?,?,?,?,?,?)}");
@@ -84,5 +88,29 @@ public class Metodo {
             Conexion.desconectar();
             return false;
         }
+    }
+
+    /**
+     * Método llama al procedimiento de CRUD de JefeDepto y devuelve Booleano
+     *
+     * @param RUT
+     * @return Boolean
+     */
+    public boolean BuscarJefe(String RUT) {
+        boolean registros = false;
+        String sql = "SELECT count(1) as total FROM JEFEDEPTO where RUT='" + RUT + "';";
+        try {
+            PreparedStatement st = Conexion.conectar().prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            int dato = rs.getInt("total");
+            rs.close();
+            if (dato > 0) {
+                registros = true;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return registros;
     }
 }
